9fb78f49036f4a040fbad3a6c8b32b54fb72ac2b
test <652451899@qq.com>
2020-09-09 22:51:05 +0800

    new .gitignore

---
+++ aa.java
@@ -0,0 +1,15 @@
+public class aa
+{
+    public static void main(String[] args)
+    {
+       double x=999.999;
+       int a=(int)x;
+       byte b=(byte)x;
+       long z=Math.round(x);
+       byte y =(byte)Math.round(x);
+       System.out.println(y);
+       System.out.println(z);
+       System.out.println(a);
+       System.out.println(b);
+     }
+}
